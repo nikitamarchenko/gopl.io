31c2e7b66260261199663a1cef6b8717e1d6f933
Nikita Marchenko <nikita.marchenko@gmail.com>
2023-12-09 20:51:36 +0200

    add ex 3.1 3.2 fix typo

--- ch1/ex1.1/main.go
+++ ch1/ex1.1/main.go
@@ -1,4 +1,4 @@
-// Exercis e 1.1: Modify the echo program to also print os.Args[0], 
+// Exercise 1.1: Modify the echo program to also print os.Args[0], 
 // the name of the command that invoked it.
 
 package main
