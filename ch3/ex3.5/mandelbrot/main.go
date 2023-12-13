d2061cc91a402e8aac3f308e03c2229a3731505c
Nikita Marchenko <nikita.marchenko@gmail.com>
2023-12-13 18:50:06 +0200

    cleanup ex3.5

--- ch3/ex3.5/mandelbrot/main.go
+++ ch3/ex3.5/mandelbrot/main.go
@@ -21,9 +21,7 @@ func main() {
 		width, height          = 1024, 1024
 	)
 
-	// img := image.NewRGBA(image.Rect(0, 0, width, height))
-
-	img := image.NewCMYK(image.Rect(0, 0, width, height))
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
 
 	for py := 0; py < height; py++ {
 		y := float64(py)/height*(ymax-ymin) + ymin
@@ -71,45 +69,10 @@ func mandelbrot(z complex128) color.Color {
 				1.5),
 			360.0),
 		50,
-		((float64(n)/float64(iterations))*100),
+		((float64(n) / float64(iterations)) * 100),
 	)
 }
 
-//!-
-
-// Some other interesting functions:
-
-func acos(z complex128) color.Color {
-	v := cmplx.Acos(z)
-	blue := uint8(real(v)*128) + 127
-	red := uint8(imag(v)*128) + 127
-	return color.YCbCr{192, blue, red}
-}
-
-func sqrt(z complex128) color.Color {
-	v := cmplx.Sqrt(z)
-	blue := uint8(real(v)*128) + 127
-	red := uint8(imag(v)*128) + 127
-	return color.YCbCr{128, blue, red}
-}
-
-// f(x) = x^4 - 1
-//
-// z' = z - f(z)/f'(z)
-//
-//	= z - (z^4 - 1) / (4 * z^3)
-//	= z - (z - 1/z^3) / 4
-func newton(z complex128) color.Color {
-	const iterations = 37
-	const contrast = 7
-	for i := uint8(0); i < iterations; i++ {
-		z -= (z - 1/(z*z*z)) / 4
-		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
-			return color.Gray{255 - contrast*i}
-		}
-	}
-	return color.Black
-}
 
 func hueToRGB(v1, v2, h float64) float64 {
 	if h < 0 {
