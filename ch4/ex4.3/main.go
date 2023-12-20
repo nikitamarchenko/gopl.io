d1c113e673f588c9e59f4c60ccdd19ba0f16c3d3
Nikita Marchenko <nikita.marchenko@gmail.com>
2023-12-20 23:47:53 +0200

    add ex4.3

---
+++ ch4/ex4.3/main.go
@@ -0,0 +1,23 @@
+/*
+Exercise 4.3: Rewrite reverse to use an array pointer instead of a slice.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	reverse(&a)
+	fmt.Println(a) // "[5 4 3 2 1 0]"
+}
+
+// len is part of array type so we must pass concrete size
+func reverse(s *[6]int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
