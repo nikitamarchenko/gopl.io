a63b665e3f9fbe53b89f4bbe14710b51731d1b2a
Nikita Marchenko <nikita.marchenko@gmail.com>
2024-01-11 11:57:05 +0200

    add ex5.9

---
+++ ch5/ex5.9/main.go
@@ -0,0 +1,63 @@
+/*
+Exercise 5.9: Write a function expand(s string, f func(string) string) string
+that replaces each substring “$foo” within s by the text returned by f("foo").
+*/
+
+package main
+
+import (
+	"bytes"
+	"strings"
+	"unicode"
+)
+
+func trivial_expand(s string, f func(string) string) string {
+	words := strings.Split(s, " ")
+
+	for i, w := range words {
+		if after, found := strings.CutPrefix(w, "$"); found {
+			words[i] = f(after)
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+func expand(s string, f func(string) string) string {
+
+	const (
+		scanning byte = iota
+		collect
+	)
+
+	var buf, val bytes.Buffer
+	state := scanning
+	for _, r := range s {
+		switch state {
+		case scanning:
+			if r == '$' {
+				state = collect
+			} else {
+				buf.WriteRune(r)
+			}
+		case collect:
+			if r == '$' {
+				buf.WriteString(f(val.String()))
+				val.Reset()
+			} else if unicode.IsSpace(r) {
+				buf.WriteString(f(val.String()))
+				buf.WriteRune(r)
+				val.Reset()
+				state = scanning
+			} else {
+				val.WriteRune(r)
+			}
+		}
+	}
+
+	if val.Len() > 0 {
+		buf.WriteString(f(val.String()))
+	}
+
+	return buf.String()
+}
---
+++ ch5/ex5.9/main_test.go
@@ -0,0 +1,52 @@
+package main
+
+import "testing"
+
+func TestExpand(t *testing.T) {
+
+	tests := []struct {
+		name   string
+		input  string
+		f      func(string) string
+		output string
+	}{
+		{name: "empty"},
+		{name: "basic",
+			input: "$foo",
+			f: func(s string) string {
+				return "baz"
+			},
+			output: "baz",
+		},
+		{name: "basic",
+			input: "$foo $test",
+			f: func(s string) string {
+				return s + "!"
+			},
+			output: "foo! test!",
+		},
+		{name: "intermediate",
+			input: " _  __$foo$test  _",
+			f: func(s string) string {
+				return s + "!"
+			},
+			output: " _  __foo!test!  _",
+		},
+		{name: "advanced",
+			input: "  start$你好$世界 end ",
+			f: func(s string) string {
+				return "(" + s + ")"
+			},
+			output: "  start(你好)(世界) end ",
+		},
+	}
+
+	for _, test := range tests {
+		if r := expand(test.input, test.f); r != test.output {
+			t.Errorf("error in %s:\n\tinput  : [%s]\n\toutput : [%s]\n\tresult = [%s]",
+				test.name, test.input, test.output, r)
+		} else {
+			t.Logf("%s: pass", test.name)
+		}
+	}
+}
