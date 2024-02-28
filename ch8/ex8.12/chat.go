f80f97be504e04ed8d04f6c7cac2f0c53b12571a
Nikita Marchenko <nikita.marchenko@gmail.com>
2024-02-28 14:19:38 +0200

    add ex8.12

---
+++ ch8/ex8.12/chat.go
@@ -0,0 +1,108 @@
+/*
+
+Exercise 8.12: Make the broadcaster announce the current set of clients to each 
+new arrival. This requires that the clients set and the entering and leaving 
+channels record the client name too.
+
+*/
+
+
+
+// Chat is a server that lets clients chat with each other.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+//!+broadcaster
+type client struct {
+	name string
+	ch chan<- string // an outgoing message channel
+}
+
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string) // all incoming client messages
+)
+
+func broadcaster() {
+	clients := make(map[client]bool) // all connected clients
+	for {
+		select {
+		case msg := <-messages:
+			// Broadcast incoming message to all
+			// clients' outgoing message channels.
+			for cli := range clients {
+				cli.ch <- msg
+			}
+
+		case cli := <-entering:
+			clients[cli] = true
+			cli.ch <- "Users:"
+			for c := range clients {
+				cli.ch <- fmt.Sprintf("\t%s", c.name)
+			}
+
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli.ch)
+		}
+	}
+}
+
+//!-broadcaster
+
+//!+handleConn
+func handleConn(conn net.Conn) {
+	ch := make(chan string) // outgoing client messages
+	go clientWriter(conn, ch)
+	who := conn.RemoteAddr().String()
+	client := client{who, ch}
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+	entering <- client
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+	}
+	// NOTE: ignoring potential errors from input.Err()
+
+	leaving <- client
+	messages <- who + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		
+		fmt.Fprintln(conn, msg) // NOTE: ignoring network errors
+	}
+}
+
+//!-handleConn
+
+//!+main
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+//!-main
