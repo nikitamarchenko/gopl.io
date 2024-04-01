05aaca81a5153820c5e79b0fe743a5d735fcd73b
Nikita Marchenko <nikita.marchenko@gmail.com>
2024-04-01 18:10:10 +0300

    add ex12.13

---
+++ ch12/ex12.13/sexpr_test.go
@@ -0,0 +1,64 @@
+/*
+
+Exercise 12.13: Modify the S-expression encoder (§12.4) and decoder (§12.6) so
+that they honor the sexpr:"..." field tag in a similar manner to
+encoding/json (§4.5).
+
+*/
+
+package sexpr
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	type Movie struct {
+		Title    string            `sexpr:"title"`
+		Subtitle string            `sexpr:"subtitle"`
+		Year     int               `sexpr:"year"`
+		Actor    map[string]string `sexpr:"actor"`
+		Oscars   []string          `sexpr:"oscars"`
+		Sequel   *string           `sexpr:"sequel"`
+	}
+	strangelove := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+	}
+
+	// Encode it
+	data, err := Marshal(strangelove)
+	if err != nil {
+		t.Fatalf("Marshal failed: %v", err)
+	}
+	t.Logf("Marshal() = %s\n", data)
+
+	// Decode it
+	var movie Movie
+	if err := Unmarshal(data, &movie); err != nil {
+		t.Fatalf("Unmarshal failed: %v", err)
+	}
+	t.Logf("Unmarshal() = %+v\n", movie)
+
+	// Check equality.
+	if !reflect.DeepEqual(movie, strangelove) {
+		t.Fatal("not equal")
+	}
+
+}
